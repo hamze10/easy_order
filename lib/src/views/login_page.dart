230804d11d76c65053f6b9a7c45d632c7ddead7f
hamze10 <hamza.mahmoudi@student.vinci.be>
2020-06-18 14:15:19 +0200

    adding form without bloc architecture yet

--- lib/src/views/login_page.dart
+++ lib/src/views/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_order/src/models/login.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,6 +7,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _user = Login();
+
   Text customText({String content, double size}) => Text(
         content,
         style: TextStyle(
@@ -14,7 +18,11 @@ class _LoginPageState extends State<LoginPage> {
         ),
       );
 
-  TextFormField customTFF({IconData icon, String text, String validatorText}) =>
+  TextFormField customTFF(
+          {IconData icon,
+          String text,
+          String validatorText,
+          bool isEmail = false}) =>
       TextFormField(
         decoration: InputDecoration(
           enabledBorder: UnderlineInputBorder(
@@ -22,32 +30,66 @@ class _LoginPageState extends State<LoginPage> {
               color: Colors.white,
             ),
           ),
-          icon: Icon(
-            icon,
-            color: Colors.white,
-          ),
           labelText: text,
           labelStyle: TextStyle(
             color: Colors.white,
           ),
+          errorStyle: TextStyle(
+            color: Colors.red[300],
+          ),
+          focusedErrorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red[300],
+            ),
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red[300],
+            ),
+          ),
+          suffixIcon: Icon(
+            icon,
+            color: Colors.white,
+          ),
         ),
-        validator: (value) => value.isEmpty ? validatorText : null,
+        validator: (value) {
+          if (value.isEmpty) return validatorText;
+          if (isEmail) {
+            if (!RegExp(
+                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                .hasMatch(value))
+              return 'Veuillez entrer une adresse email valide';
+          } else {
+            if (value.length < 8)
+              return 'Votre mot de passe doit contenir 8 caractères minimum.';
+            if (!value.contains(RegExp(r'\d')))
+              return 'Votre mot de passe doit contenir au moins un chiffre.';
+          }
+          return null;
+        },
+        onChanged: (value) =>
+            isEmail ? _user.email = value : _user.password = value,
         style: TextStyle(
           color: Colors.orange[200],
         ),
+        obscureText: !isEmail,
       );
 
-  ButtonTheme customButton(
-          {double size = 220,
-          String content,
-          Color color,
-          double fontSize = 16.0,
-          Color textColor = Colors.black}) =>
+  ButtonTheme customButton({
+    double size = 500,
+    String content = "",
+    Color color,
+    double fontSize = 16.0,
+    Color textColor = Colors.black,
+    Function onPress,
+  }) =>
       ButtonTheme(
         minWidth: size,
         buttonColor: color,
         child: RaisedButton(
-          onPressed: () {},
+          onPressed: () {
+            onPress();
+          },
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: Text(
@@ -66,12 +108,12 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red[400],
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('images/background2.png'),
+              image: AssetImage('images/background3.jpg'),
               fit: BoxFit.cover,
             ),
           ),
@@ -82,66 +124,82 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
                 children: <Widget>[
                   Expanded(
-                    child: Image.asset('images/logo.webp'),
+                    child: Image.asset('images/logo.png'),
                   ),
                   Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        customTFF(
-                          icon: Icons.email,
-                          text: 'EMAIL',
-                          validatorText: 'Veuillez entrer un email.',
-                        ),
-                        customTFF(
-                          icon: Icons.lock,
-                          text: 'MOT DE PASSE',
-                          validatorText: 'Veuillez entrer un mot de passe.',
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: <Widget>[
-                              customText(
-                                  content: 'Mot de passe oublié ? ', size: 11),
-                            ],
+                    flex: 2,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          customTFF(
+                            icon: Icons.email,
+                            text: 'EMAIL',
+                            validatorText: 'Veuillez entrer un email.',
+                            isEmail: true,
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0),
-                          child: customButton(
-                            color: Colors.white,
-                            content: 'Connexion',
+                          customTFF(
+                            icon: Icons.lock,
+                            text: 'MOT DE PASSE',
+                            validatorText: 'Veuillez entrer un mot de passe.',
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0),
-                          child: customButton(
-                            color: Colors.red[600],
-                            content: 'Google',
-                            textColor: Colors.white,
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 10.0,
+                              bottom: 10.0,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                customText(
+                                    content: 'Mot de passe oublié ? ',
+                                    size: 13),
+                              ],
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              customText(
-                                content: 'Pas de compte ? ',
-                                size: 12.0,
-                              ),
-                              Text(
-                                'créer un nouveau compte.',
-                                style: TextStyle(
-                                  color: Colors.orange[200],
-                                  fontWeight: FontWeight.bold,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: customButton(
+                                color: Colors.white,
+                                content: 'Connexion',
+                                onPress: () {
+                                  //Formulaire valide
+                                  if (_formKey.currentState.validate()) {
+                                    print("email : " + _user.email);
+                                    print("password : " + _user.password);
+                                  }
+                                }),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: customButton(
+                              color: Colors.red[600],
+                              content: 'Google',
+                              textColor: Colors.white,
+                              onPress: () => print("Test google"),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                customText(
+                                  content: 'Pas de compte ? ',
+                                  size: 12.0,
                                 ),
-                              ),
-                            ],
+                                Text(
+                                  'créer un nouveau compte.',
+                                  style: TextStyle(
+                                    color: Colors.orange[200],
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ],
